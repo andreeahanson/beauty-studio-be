0cec5bf74377999b53b424ffa46625e3689f14cb
Andreea Hanson <andreea.hanson7@gmail.com>
2020-01-23 23:39:50 -0700

    Add GET response

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3001);
+
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 //
 
 server.listen(3001, () => {
@@ -17,3 +22,15 @@ server.on('request', (request, response) => {
   response.write('Beauty Studio Back-End');
   response.end();
 })
+
+//
+app.get('/api/v1/beauty_products', (request, response) => {
+  database('beauty_products').select()
+    .then((beauty_products) => {
+      response.status(200).json(beauty_products);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+//
