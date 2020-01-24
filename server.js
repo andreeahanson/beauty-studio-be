f1a88d322bf60292549def3699f285d59b8d6a44
Andreea Hanson <andreea.hanson7@gmail.com>
2020-01-23 23:35:00 -0700

    Add express

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http');
 const url = require('url');
 const server = http.createServer();
+//
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3001);
+//
 
 server.listen(3001, () => {
   console.log('The HTTP server is listening at Port 3001');
