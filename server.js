ba8ae6ba8c29fa2e4f76f454470dc33d33bbc8e3
Andreea Hanson <andreea.hanson7@gmail.com>
2020-01-24 20:32:27 -0700

    Add get method for all notes

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 app.use(cors());
 app.use(express.json());
-const environment = process.env.NODE_ENV || "development";
-const configuration = require("./knexfile")[environment];
-const database = require("knex")(configuration);
-app.locals.title = "Beauty Products & Notes";
-app.set("port", process.env.PORT || 3001);
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+app.locals.title = 'Beauty Products & Notes';
+app.set('port', process.env.PORT || 3001);
 
 app.listen(3001, () => {
-  console.log("The HTTP server is listening at Port 3001");
+  console.log('The HTTP server is listening at Port 3001');
 });
 
-app.get("/", (request, response) => {
-  response.send("Beauty Products & Notes");
+app.get('/', (request, response) => {
+  response.send('Beauty Products & Notes');
 });
 
-app.get("/api/v1/beauty_products", (request, response) => {
-  database("beauty_products")
+app.get('/api/v1/beauty_products', (request, response) => {
+  database('beauty_products')
     .select()
     .then(beauty_products => {
       response.status(200).json(beauty_products);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/notes', (request, response) => {
+  database('notes')
+    .select()
+    .then(notes => {
+      response.status(200).json(notes);
     })
     .catch(error => {
       response.status(500).json({ error });
@@ -44,10 +55,25 @@ app.get('/api/v1/beauty_products/:id', (request, response) => {
     });
 });
 
-app.post("/api/v1/beauty_products", (request, response) => {
+app.get('/api/v1/beauty_products/:id/notes', async (request, response) => {
+  try {
+    const id = request.params.id;
+    const validProduct = await database('beauty_products').where('id', id);
+    if(validProduct.length) {
+      const notes = await database('notes').where('beauty_product_id', id).select();
+      response.status(200).json(notes)
+    } else {
+      response.status(404).json(`The beauty product with the id of ${id} doesn't exist`)
+    }
+  } catch (error) {
+    response.status(500).json({ error });
+  }
+});
+
+app.post('/api/v1/beauty_products', (request, response) => {
   const product = request.body;
 
-  for (let requiredParameter of ["name", "brand"]) {
+  for (let requiredParameter of ['name', 'brand']) {
     if (!product[requiredParameter]) {
       return response.status(422).send({
         error: `Expected format: { name: <String>, author: <String> }. You're missing a ${requiredParameter} property.`
@@ -55,8 +81,8 @@ app.post("/api/v1/beauty_products", (request, response) => {
     }
   }
 
-  database("beauty_products")
-    .insert(product, "id")
+  database('beauty_products')
+    .insert(product, 'id')
     .then(product => {
       response.status(201).json({ id: product[0] });
     })
@@ -64,5 +90,26 @@ app.post("/api/v1/beauty_products", (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.post('/api/v1/beauty_products/:id/notes', (request, response) => {
+  const note = request.body;
+
+  for (let requiredParameter of ['note']) {
+    if(!note[requiredParameter]) {
+      return response.status(422).send({
+        error: `Expected format: { note: <String> }. You are missing the ${requiredParameter} property.`
+      });
+    }
+  }
+
+  database('notes')
+    .insert(note, 'id')
+    .then(note => {
+      response.status(201).json({ id: note[0] });
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
 
 
