0a81f7e8bbab1ee161aab3322607645bc17c06e2
Andreea Hanson <andreea.hanson7@gmail.com>
2020-02-04 01:09:47 -0700

    Pass GET beauty_products happy path test

--- server.test.js
+++ server.test.js
@@ -4,16 +4,17 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-describe('API', () => {
-  beforeEach(async() => {
-    await database.seed.run();
-  })
-});
+// describe('API', () => {
+//   beforeEach(async () => {
+//     await database.seed.run();
+//   })
+// });
 
 describe('GET / beauty_products', () => {
   describe('happy path', () => {
-    it('should return a status code of 200', () => {
-
+    it('should return a status code of 200', async () => {
+      const res = await request(app).get('/api/v1/beauty_products');
+      expect(res.status).toBe(200);
     })
   })
 })
